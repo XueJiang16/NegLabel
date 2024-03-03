4c3b786fcfd6440d11c0513523b64381ff252ba3
jjjjump <40789479+XueJiang16@users.noreply.github.com>
2024-03-03 22:15:12 +0800

    Update __init__.py

--- mmcls/models/classifiers/__init__.py
+++ mmcls/models/classifiers/__init__.py
@@ -2,7 +2,6 @@
 from .base import BaseClassifier
 from .image import ImageClassifier
 from .vit_lt import VitClassifier
-from .multi_modal import CLIPScalableClassifier, \
-    HuggingFaceCLIPScalableClassifier, HuggingFaceSimpleScalableClassifier, CLIPZOCClassifier
+from .multi_modal import CLIPScalableClassifier
 
 __all__ = ['BaseClassifier', 'ImageClassifier', 'VitClassifier', 'CLIPScalableClassifier']
